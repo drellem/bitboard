6d5ecfc05e2bcc27bf510418ae413433dcb0117e
Daniel Miller <gate46dmiller@gmail.com>
2021-05-27 21:42:42 -0500

    Nicer types, zero proc

--- src/bitboard.nim
+++ src/bitboard.nim
@@ -17,16 +17,27 @@ const WORD_LEN* = 8 # size of bitboard is 2^BIT_LEN
 const BIT_LEN* = 256 # todo import math and use "proc(2, WORD_LEN)"
 const INT_SZ* = 32 # number of bits in an int
 
+type Board = array[WORD_LEN, uint]
 
-proc get*(b :  ptr array[WORD_LEN, uint], i : int ) : bool  =
-  # gets the kth value from the bitboard
+proc newBitboard*() : ptr Board =
+  # Allocates a a new Bitboard.
+  return cast[ptr Board](alloc0(sizeof(Board)))
+
+proc zero*(b : ptr Board) =
+  # Zeroes out a bitboard
+  for i in 0..WORD_LEN-1:
+    b[i] = 0
+  
+proc get*(b :  ptr Board, i : int ) : bool  =
+  # gets the ith value from the bitboard
   if(i < 0 or i >= BIT_LEN):
     raise newException(IndexDefect, "Invalid index " & $i)
   var word = i div INT_SZ
   var bit = i mod INT_SZ
   return b[word].testBit(bit)
 
-proc set*(b : ptr array[WORD_LEN, uint], i : int, val : bool ) : void =
+proc set*(b : ptr Board, i : int, val : bool ) : void =
+  # Sets the ith value of the bitboard
   if(i < 0 or i >= BIT_LEN):
     raise newException(IndexDefect, "Invalid var i = " & $i)
   var word = i div INT_SZ
@@ -38,16 +49,14 @@ proc set*(b : ptr array[WORD_LEN, uint], i : int, val : bool ) : void =
     wordVal.clearBit(bit)
   b[word] = wordVal
 
-proc bbOr*(b : ptr array[WORD_LEN, uint], b2 : ptr array[WORD_LEN, uint]): array[WORD_LEN, uint] =
-    var ret : array[WORD_LEN, uint]
-    for i in 0..WORD_LEN-1:
-      ret[i] = b[i] or b2[i]
-    return ret
+proc bbOr*(b : ptr Board, b2 : ptr Board) =
+  # Takes the union of the two bitboards and stores in the first
+  for i in 0..WORD_LEN-1:
+    b[i] = b[i] or b2[i]
 
-proc bbAnd*(b : ptr array[WORD_LEN, uint], b2 : ptr array[WORD_LEN, uint]): array[WORD_LEN, uint] =
-    var ret : array[WORD_LEN, uint]
-    for i in 0..WORD_LEN-1:
-      ret[i] = b[i] and b2[i]
-    return ret
+proc bbAnd*(b : ptr Board, b2 : ptr Board) =
+  # Takes the intersection of the two bitboards and stores in the first
+  for i in 0..WORD_LEN-1:
+    b[i] = b[i] and b2[i]
 
   
--- test/test.nim
+++ test/test.nim
@@ -2,27 +2,34 @@
 
 import bitboard
 
-var bb : array[WORD_LEN, uint]
-var bb2 : array[WORD_LEN, uint]
+# Allocate new bitboards
 
-var bitboard1 = addr(bb)
-var bitboard2 = addr(bb2)
+var bitboard1 = newBitboard()
+var bitboard2 = newBitboard()
 
 # initialize
 
-for i in 0..WORD_LEN-1:
-  bitboard1[i] = 0
-  bitboard2[i] = 0
+bitboard1.zero()
+bitboard2.zero()
 
 bitboard1.set(64, true)
 bitboard2.set(65, true)
 
-bb = bitboard1.bbOr(bitboard2)
-bb2 = bitboard1.bbAnd(bitboard2)
+# Do some operations
+
+bitboard1.bbOr(bitboard2)
+bitboard2.bbAnd(bitboard1)
+
+# Test
 
 assert bitboard1.get(64) == true
 assert bitboard1.get(65) == true
 assert bitboard2.get(64) == false
 assert bitboard2.get(65) == true
+
+# Free the memory
+
+dealloc(bitboard1)
+dealloc(bitboard2)
 
 echo "Test complete"
